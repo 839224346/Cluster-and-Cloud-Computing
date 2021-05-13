84db87456e8f8bc964677a29a77598975e26c272
Yi <42195839+839224346@users.noreply.github.com>
2021-05-14 03:06:08 +1000

    Add files via upload

--- backend/test.py
+++ backend/test.py
@@ -5,7 +5,7 @@ current_time = time.localtime()
 
 today = time.strftime('%Y-%-m-%-d', current_time)
 
-file_name = today + ".txt"
+file_name = today +str(current_time) + ".txt"
 
 output = open(file_name, "w")
 
