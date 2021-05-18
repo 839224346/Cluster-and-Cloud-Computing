daa3c04073423865216376650b31e06ffdfbd043
CONGRANLI <907112284@qq.com>
2021-05-18 16:25:05 +0800

    feat: modify infoWindow Css

--- front-end/src/InfoWindow.js
+++ front-end/src/InfoWindow.js
@@ -5,7 +5,7 @@ import { Divider, Col, Row } from 'antd';
 function InfoWindow(props){
     let {name,GP_num,Education,covid_attention,level_advanced,population} = props.params
     const content = (
-        <div style={{maxHeight:'1000px', weight:'250px', minWidth:'500px', textAlign:'center'}}>
+        <div style={{maxHeight:'1000px', weight:'250px', minWidth:'550px', textAlign:'center'}}>
             <h1 style={{color:'#0e4381'}}>{name}</h1>
             <Row>
             <Col span={8}>
