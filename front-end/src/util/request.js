980ee4fa5cf805dfc73f025d0ec441b169a43013
Yi <42195839+839224346@users.noreply.github.com>
2021-05-23 20:42:17 +1000

    Update request.js

--- front-end/src/util/request.js
+++ front-end/src/util/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const request = axios.create({
 //   baseURL: "http://167.172.11.217/",
-  baseURL: "http://172.26.132.20:6100/",
+  baseURL: "http://localhost:6100/",
   timeout: 10000,
   // headers: { "auth-token": accessTokenKey },
 })
