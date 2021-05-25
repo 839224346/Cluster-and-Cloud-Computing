8fece36660bb42fcf3ef33743326b9c22b765bf4
Yi <42195839+839224346@users.noreply.github.com>
2021-05-26 04:04:15 +1000

    Add files via upload

--- crawler_loader/test-data-update.py
+++ crawler_loader/test-data-update.py
@@ -9,16 +9,14 @@ except:
     db = couch['realtime']
     
 
-# if len(txt_files) != 1:
-#     raise ValueError('should be only one txt file in the current directory')
+filename = 'sample-10.txt'
 
-
-filename = 'sample-1000.txt'
 with open(filename, 'r') as f:
     count = 0
     tweet_list =[]
     for line in f:
         count += 1
+
         tweet = json.loads(line)
         timestamp = int(datetime.strptime(tweet['created_at'], "%Y-%m-%d %H:%M:%S").timestamp())
         tweet['created_at']  = timestamp
